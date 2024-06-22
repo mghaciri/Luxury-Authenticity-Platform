b86f413b910569ae596d1a6f5c2e5f1ccf76ce4a
Mohammed Ghaciri <mghaciri@gmail.com>
2024-06-22 20:26:09 +0200

    Add Change Workflow status buttons

--- front/app/page.js
+++ front/app/page.js
@@ -1,13 +1,26 @@
 "use client";
 import Home from "@/components/Home";
-import WhiteListRegistration from "@/components/WhiteListRegistration";
+//import WhiteListRegistration from "@/components/WhiteListRegistration";
+import VotingStartSessionButton from "@/components/VotingStartSessionButton";
+import VotingSessionEndButton from "@/components/VotingSessionEndButton";
+import ProposalRegistrationStartButton from "@/components/ProposalRegistrationStartButton";
+import ProposalRegistrationEndButton from "@/components/ProposalRegistrationEndButton";
+import TallyVotesButton from "@/components/TallyVotesButton";
+
+import Events from "@/components/Events";
+
 import Image from "next/image";
 
 const App = () => {
   return (
     <>
       <Home />
-      <WhiteListRegistration />
+      <ProposalRegistrationStartButton />
+      <ProposalRegistrationEndButton />
+      <VotingStartSessionButton />
+      <VotingSessionEndButton />
+      <TallyVotesButton />
+      <Events />
       <Home />
     </>
   );
