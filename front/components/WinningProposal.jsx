4ec2f72cc33a31c2d629df8bb0e5387a67559947
Mohammed Ghaciri <mghaciri@gmail.com>
2024-06-23 22:20:38 +0200

    Fix affichage

--- front/components/WinningProposal.jsx
+++ front/components/WinningProposal.jsx
@@ -1,31 +1,26 @@
 "use client";
-import { useState, useEffect } from "react"; // Ensure useEffect is imported
+import { useEffect } from "react"; // Ensure useEffect is imported
 import { Button } from "flowbite-react"; // Removed unused import 'Card'
 import { useReadContract } from "wagmi";
 import { contractAbi, contractAddress } from "@/constants";
 
 function WinningProposal() {
-//  const [winningProposalID, setwinningProposalID] = useState("");
-  console.log("Dans WinningProposal");
+
   const { data: winningProposalID, refetch } = useReadContract({
     address: contractAddress,
     abi: contractAbi,
     functionName: 'winningProposalID',
   });
-  console.log(winningProposalID);
-//  setwinningProposalID(result);
 
   useEffect(() => {
     console.log("Dans useEffect");
     refetch();
-    console.log(winningProposalID);
   }, [refetch]);
+
 
   // Function to handle button click for refetching the winning proposal ID
   const handleRefetch = () => {
-    console.log("Dans handleRefetch");
     refetch();
-    console.log(winningProposalID);
   };
 
   return (
@@ -33,7 +28,7 @@ function WinningProposal() {
       <h2 className="text-white xl:text-5xl lg:text-3xl text-2xl font-semibold leading-loose text-center mb-6">
         3. Winning Proposal
       </h2>
-      <p className='text-black'>Winning Proposal : {winningProposalID.toString() ?? 'Loading...'}</p>
+      <p className='text-black'>Winning Proposal : {(winningProposalID ?? 'Loading...').toString()}</p>
       <Button gradientMonochrome="purple" onClick={handleRefetch}>
         Show Winning Proposal
       </Button>
