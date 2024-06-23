ed0b1950c039e532d48330fc75fac2f6bd469977
Ty HA <124097666+Ty-HA@users.noreply.github.com>
2024-06-24 00:26:58 +0200

    Update Home.jsx

--- front/components/Home.jsx
+++ front/components/Home.jsx
@@ -7,8 +7,7 @@ const Home = () => {
       <section className="flex flex-col justify-around items-center pb-6 w-full bg-[#0a1835] h-[50vh]">
         <div className="flex flex-row justify-center items-center">
         <h2
-          style={{ lineHeight: "1.2" }}
-          className="text-white mt-6 xl:text-5xl lg:text-3xl text-2xl font-semibold leading-loose"
+          className="text-white mt-24 md:mt-0 xl:text-5xl lg:text-3xl text-2xl font-semibold leading-loose"
         >
           Welcome to our Voting Dapp
         </h2>
