8ef01ee7b52b60097208812780f0162d34eca4ce
Ty HA <bichty.ha@gmail.com>
2024-06-23 23:52:28 +0200

    update Home

--- front/components/Home.jsx
+++ front/components/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
           style={{ lineHeight: "1.2" }}
           className="text-white mt-6 xl:text-5xl lg:text-3xl text-2xl font-semibold leading-loose"
         >
-          Welcome on our Voting Dapp
+          Welcome to our Voting Dapp
         </h2>
         <img src="/home2.png" alt="Home" style={{ width: '400px' }} />
         </div>
