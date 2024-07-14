a4adb1fe970e84897b1198e9c15a173d9b6126d8
Mohammed Ghaciri <mghaciri@gmail.com>
2024-07-14 13:52:37 +0200

    Optimisation affichage

--- front/components/Montre.jsx
+++ front/components/Montre.jsx
@@ -77,20 +77,50 @@ function Montre(product) {
   return (
     <>
       <div key={product.id} className="group relative">
-        <h3 className="text-sm text-center">
-          {ownerOf?.toString().substring(0,15)}...
-        </h3>            
+        <h3 className="text-sm text-center" >
+          <a target="_blank" href={"https://sepolia.etherscan.io/token/" + contractAddress.toString() + "?a=" + ownerOf?.toString()} >
+            {ownerOf?.toString().substring(0,15)}...
+          </a>
+        </h3>
+        <br></br>   
         <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+          <a href={ipfsURI + montre.image} target="_blank">
             <img
             alt={montre.description}
             src={ipfsURI + montre.image}
+            href={ipfsURI + montre.image}
             className="h-full w-full object-cover object-center lg:h-full lg:w-full"
             />
+          </a>
         </div>
+        <br></br>  
         <h3 className="text-sm text-center" >
-            {montre.description}
+          {montre.description}
         </h3>
-        
+        <h3 className="text-sm text-center" >
+            Model :
+        </h3>
+        <h3 className="text-sm text-center" >
+            S/N : 
+        </h3>
+        <h3 className="text-sm text-center" >
+          Weight : 
+        </h3>
+        <h3 className="text-sm text-center" >
+            Diameter : 
+        </h3>
+        <h3 className="text-sm text-center" >
+            Material : 
+        </h3>
+
+        <h3 className="text-sm text-center" >
+            Genre : 
+        </h3>
+        <br></br>          <br></br>  
+        <a target="_blank" href={"https://sepolia.etherscan.io/nft/" + contractAddress.toString() + "/" + product.id} >
+            History
+          </a>
+        <br></br>          <br></br>  
         {isCurrentOwner && <section className="flex justify-center items-center w-full bg-blue-950">
             <input
             type="string"
@@ -99,9 +129,6 @@ function Montre(product) {
             className="input input-bordered input-primary w-full max-w-xs text-black items-center"
             placeholder="Enter destination address"
             />
-            <div className="mt-1 text-sm">
-                <Button onClick={() => handleTransfer(product.id)}>Transfer</Button>
-            </div>
           </section>
         }
         <div className="mt-4 w-80">
